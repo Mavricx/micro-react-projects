969f1a38724667d405a2a51eafe92f266070838c
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2025-08-19 04:47:04 +0530

    building the todo app with context api

---
+++ todoApp/src/contexts/TodoContext.js
@@ -0,0 +1,20 @@
+import {createContext, useContext} from 'react';
+export const TodoContext=createContext({
+    todos:[
+        {
+            id:Date.now(),
+            todo:"Todo Message",
+            completed:false,
+        }
+    ],
+    addTodo:(todo)=>{},
+    updateTodo:(id,todo)=>{},
+    deleteTodo:(id)=>{},
+    toggleComplete:(id)=>{}
+});
+
+export const useTodo=()=>{
+    return useContext(TodoContext);
+}
+
+export const TodoProvider=TodoContext.Provider;
